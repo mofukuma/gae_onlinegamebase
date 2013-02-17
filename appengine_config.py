9e62f831a724986d375884d52ab8ff22b5817539
mofukuma <mofukuma@db1.(none)>
2013-02-18 03:03:12 +0900

    formencode/ 	index.yaml 	k/util.py 	models.py 	myconfig.py

--- appengine_config.py
+++ appengine_config.py
@@ -1,10 +1,62 @@
-#for profile appstats
+#!-*- coding:utf-8 -*-
 
+"""
 import os
 
+ON_DEV = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
+
+engineauth = {
+    'login_uri': '/', #認証ないとき
+    'success_uri': '/', #認証成功後
+    'secret_key': 'kumakama109481-09234',
+    'user_model': 'models.AccountDB', #自家アカウントのモデル
+}
+
+engineauth['provider.google'] = {
+    'client_id': '673072897993.apps.googleusercontent.com',
+    'client_secret': '4bNhP7gETK0DLOvIhC0rO39b',
+    'api_key': '',
+    'scope': 'https://www.googleapis.com/auth/plus.me',
+    }
+
+engineauth['provider.github'] = {
+    'client_id': '7c9a74ca5fd7bdb149c2',
+    'client_secret': 'a6dbb9f8db8f881290db3bdc32c8f2ac3d5b2535',
+    }
+
+engineauth['provider.linkedin'] = {
+    'client_id': 'jfsgpazuxzb2',
+    'client_secret': 'LxGBTeCpQlb4Ad2R',
+    }
+
+engineauth['provider.twitter'] = {
+    'client_id': 'l8nfb1saEW4mlTOARqunKg',
+    'client_secret': 'LCQweRuuGndhtNWihnwiDxs9npkNRII8GAgpGkYFi5c',
+    }
+
+
+if ON_DEV:
+    # Facebook settings for Development
+    FACEBOOK_APP_KEY = '343417275669983'
+    FACEBOOK_APP_SECRET = 'fec59504f33b238a5d7b5f3b35bd958a'
+else:
+    # Facebook settings for Production
+    FACEBOOK_APP_KEY = '109551039166233'
+    FACEBOOK_APP_SECRET = 'f929abbc0c5092164df693d047f880ec'
+
+engineauth['provider.facebook'] = {
+    'client_id': FACEBOOK_APP_KEY,
+    'client_secret': FACEBOOK_APP_SECRET,
+    'scope': 'email',
+    }
+"""
+
 def webapp_add_wsgi_middleware(app):
-	#if os.environ["SERVER_SOFTWARE"].find("Development")!=-1:
-	if 1:
-		from google.appengine.ext.appstats import recording
-		app = recording.appstats_wsgi_middleware(app)
+	from google.appengine.ext.appstats import recording
+	app = recording.appstats_wsgi_middleware(app)
+	"""
+	from engineauth import middleware
+	app = middleware.AuthMiddleware(app)
+	"""
 	return app
+
--- main.py
+++ main.py
@@ -17,59 +17,297 @@ from google.appengine.api import users
 from google.appengine.api import memcache
 
 import webapp2
-from webapp2_extras import auth
-from webapp2_extras import sessions
 
-import k.micro_webapp2
-from k.models import *
+import k
+from k.util import json2 as json
+from k.util import session
+from k.util import checkauth
 
-app =  k.micro_webapp2.WSGIApplication(debug=True,
-	config={'webapp2_extras.sessions':{
-            'secret_key':'test_sskey',
-            'cookie_name':'KUMA---',
-            'session_max_age':3600,
-            'cookie_arg':{
-                'max_age':None,
-                'domain':None,
-                'path':'/',
-                'secure':None,
-                'httponly':None,
-            },
-            'default_backend':'memcache'
-            }}
+from models import *
+from myconfig import *
+
+from webapp2_extras import security
+
+app = k.util.WSGIApplication(
+debug=WEBAPP2_DEBUG,
+config=WEBAPP2_CONFIG
 )
 
-@app.route('/login')
-def loginHandler(r):
-	#Gmailのアカウント取得
-	user = users.get_current_user()
+#UUIDやパスワードでログイン。初回はアカウント作成
+@app.route("/<create_by:.*>/login")
+class pwdlogin(webapp2.RequestHandler):
+	def get(self, create_by):
+		q = self.request.GET
 
-	#ログインしてない
-	if not user:
-		login_url=users.create_login_url('/login')
-		return "<A HREF='"+login_url+"'>Sign in with Google</A>"
-	
-	#してる
-	new_userid = "GMAIL#"+str( user.user_id() )
-	try:
-		AccountDB.get_by_id(new_userid).key.delete()# テスト
-		PlayerDB.get_by_id(new_userid).key.delete()# テスト
-	except:
-		pass
-	if AccountDB.get_by_id(new_userid):#IDの重複チェック
-		return "duplicate"
-	a = AccountDB(id = new_userid, userid = new_userid, create_by = "GMAIL")
-	p = PlayerDB(id = new_userid, namae=user.nickname())  #TODO 名前入力
-	ndb.put_multi( (a, p) )#生成
-	return a
+		if create_by not in ('uuid', 'password'):
+			return
 
-@app.route('/adduser')
-def adduserHandler(r):
-	return 
+		new_userid =  create_by +":"+str( q['userid'] )
 
-@k.micro_webapp2.checkauth
-class rootHandler(k.micro_webapp2.SessionHandler):
-	@app.route('/')
+		a = AccountDB.get_by_id(new_userid)
+		if a: #IDの存在チェック
+			if security.check_password_hash(q['password'], a.password, pepper=PASSWORD_PEPPER):
+				#パスワードOK
+				p = PlayerDB.get_by_id(new_userid)
+
+				self.session = session(self.request)
+				self.session.start(new_userid, {"plkey":p.key})
+
+				#playerdb+ssid 返す
+				return webapp2.Response( json.dumps( {"result":p.to_dict()}, self.session) )
+
+			return webapp2.Response(  json.dumps( {"code":1, "message":"ログインに失敗しました。"} )  )
+
+		passh = security.create_password_hash(q['password'], pepper=PASSWORD_PEPPER)
+		a = AccountDB(id = new_userid, userid = new_userid, password = passh, create_by = create_by)
+		p = PlayerDB(id = new_userid, userid = new_userid, namae=q['namae'])
+		ndb.put_multi( (a, p) )
+
+		self.session = session(self.request)
+		self.session.start(new_userid, {"plkey":p.key})
+
+		return webapp2.Response(  json.dumps( {"result":p.to_dict()}, self.session  )  )
+
+
+#ログアウト
+@app.route("/logout")
+class pwdlogout(webapp2.RequestHandler):
+	def get(self, create_by):
+		self.session = session(self.request)
+		self.session.delete()
+		#return webapp2.Response(  json.dumps( { } )  )
+
+#指定ユーザ情報の取得
+@app.route("/getuser/<userid:.*>")
+class getuser(webapp2.RequestHandler):
+	def get(self, userid):
+		try:
+			self.session = session(self.request)
+			if userid == '': #指定がなければ自分を取得
+				userid = self.request.get('userid')
+			p = PlayerDB.get_by_id( userid )
+			self.session.save()
+			return webapp2.Response(  json.dumps( {"result":p.to_dict()}, self.session  )  )
+
+		except:
+			return webapp2.Response(  json.dumps( {"code":2, "message":"ユーザが取得できません"} )  )
+
+#フレンド申請
+@app.route("/friendrequest_to/<to_userid:.*>")
+class friendrequest_to(webapp2.RequestHandler):
+	def get(self, to_userid):
+		try:
+			self.session = session(self.request)
+			from_userid = self.session.userid
+			#TODO validate
+
+			from_player_key =self.session["plkey"]
+			to_player_key = PlayerDB.get_by_id(to_userid).key
+			f = FriendRequestDB(id=from_userid+'#'+to_userid , from_player=from_player_key, to_player=to_player_key )
+			f.put()
+
+			self.session.save()
+			return webapp2.Response(  json.dumps( self.session  )  )
+
+		except:
+			return webapp2.Response(  json.dumps( {"code":3, "message":MESSAGE_ARIENAI_ERROR} )  )
+
+#自分へのフレンドリクエストリストを取得
+@app.route("/friendrequest_list")
+class friendrequest_list(webapp2.RequestHandler):
 	def get(self):
-		return PlayerDB.get_by_id("GMAIL#185804764220139124118")
+		#try:
+			self.session = session(self.request)
+
+			#自分へのリクエストを取得
+			to_player_key = self.session["plkey"]
+			#ndbになってfilterがヤな書き方になった。注意
+			query = FriendRequestDB.query().filter(FriendRequestDB.to_player == to_player_key)
+			dat = query.fetch(100) #100人分でいいかな
+
+			#全員分のプレイヤーデータ取得
+			from_player_list = []
+			for d in dat:
+				from_player_list.append(d.from_player)
+			pldatalist = ndb.get_multi( from_player_list ) #リストにしてndb.get_multi使うと高速だよ！
+
+			self.session.save()
+			return webapp2.Response(  json.dumps( {"result":pldatalist}, self.session  )  )
+		#except:
+		#	return webapp2.Response(  json.dumps( {"code":4, "message":"フレンドリクエストリストが取得できません"} )  )
+
+#フレンド承認／却下
+@app.route("/friendrequest_yesno/<yesno:.*>/<target_userid:.*>")
+class friendrequest_yesno(webapp2.RequestHandler):
+	def get(self, yesno, target_userid):
+		#フレンドリクエストにあるかチェック
+		try:
+			self.session = session(self.request)
+			to_player_key = self.session["plkey"]
+			from_player_key = PlayerDB.get_by_id(target_userid).key
+			f_request_key = FriendRequestDB.query().filter(
+				FriendRequestDB.to_player == to_player_key).filter(
+				FriendRequestDB.from_player == from_player_key).get(keys_only=True)
+			#YES
+			if yesno == 'yes':
+				f = FriendDB(id=from_player_key.string_id()+'#'+to_player_key.string_id() , from_player=from_player_key, to_player=to_player_key )
+				self.tran_putdel(f, f_request_key)
+
+			#NO
+			elif yesno == 'no':
+				f_request_key.delete() #リクエストけすだけ
+
+			self.session.save()
+			return webapp2.Response( json.dumps( self.session ) )
+		except:
+			return webapp2.Response(  json.dumps( {"code":5, "message":MESSAGE_ARIENAI_ERROR} ) )
+
+	@ndb.transactional(retries=1, xg=True)
+	def tran_putdel(self, put1, delete1key):
+		put1.put()
+		delete1key.delete()
+
+
+#自分のフレンドリスト
+@app.route("/friend_list")
+class friend_list(webapp2.RequestHandler):
+	def get(self):
+		try:
+			self.session = session(self.request)
+
+			friend_q = FriendDB.query().filter(
+				FriendDB.to_player == self.session['plkey'] )
+			friend_keys = friend_q.fetch(200, keys_only=True)
+			dat = ndb.get_multi(friend_keys)
+
+			self.session.save()
+			return webapp2.Response(  json.dumps( {"result": dat }, self.session ) )
+
+		except:
+			return webapp2.Response(  json.dumps( {"code":6, "message":MESSAGE_ARIENAI_ERROR} ) )
+
+#フレンド削除
+@app.route("/friend_delete/<target_userid:.*>")
+class friend_delete(webapp2.RequestHandler):
+	def get(self, target_userid):
+		self.session = session(self.request)
+
+		from_player_key = PlayerDB.get_by_id(target_userid).key
+
+		f_key = FriendDB.query().filter(
+			FriendDB.to_player == self.session['plkey'] ).filter(
+			FriendDB.from_player == from_player_key
+			).get(keys_only=True)
+		f_key.delete()
+
+		self.session.save()
+		return webapp2.Response(  json.dumps( self.session ) )
+
+#シンプルメッセージ送信
+@app.route("/message_to/<target_userid:.*>")
+class message_to(webapp2.RequestHandler):
+	def get(self, target_userid):
+		self.session = session(self.request)
+
+		t = self.request.get('text')
+
+		to_player_key = PlayerDB.get_by_id(target_userid).key
+
+		msg = MessageDB(from_player=self.session['plkey'], to_player=to_player_key, text = t )
+
+		msg.put()
+
+		self.session.save()
+		return webapp2.Response( json.dumps( self.session ) )
+
+#メッセージリスト
+@app.route("/message_list")
+class message_list(webapp2.RequestHandler):
+	def get(self):
+		self.session = session(self.request)
+
+		#自分が送った/自分に送られたメール
+		mlist_keys = MessageDB.query().filter(
+			ndb.OR( MessageDB.to_player == self.session['plkey'],
+				 MessageDB.from_player == self.session['plkey'] )
+			).fetch(100, keys_only=True)
+		mdatas = ndb.get_multi(mlist_keys)
+
+		#from_playerの情報も取得
+		playerdata_keys = set() #重複しないように集合をつかう。
+		for m in mdatas:
+			playerdata_keys.add( m.from_player )
+		pldatas = ndb.get_multi(playerdata_keys)
+
+		#データの整形をしてあげる。
+		pldict = {}
+		for pl in pldatas:
+			pldict[pl.userid] = pl
+
+		mdataout = []
+		for m in mdatas:
+			mout = m.to_dict()
+			#やっぱりIDだけ渡して、クライアント側でpldictを参照する
+			#mout['from_player'] = pldict[m.from_player.string_id()]
+			mout['from_player'] = m.from_player.string_id()
+			mout['key_urlsafe'] = m.key.urlsafe()
+			mdataout.append( mout )
+
+		#新しい順にソート
+		mdataout.sort( cmp=lambda x,y: cmp(x['create_date'], y['create_date']), reverse=True )
+
+		self.session.save()
+		return webapp2.Response( json.dumps( {"result":mdataout, "pldict":pldict}, self.session ) )
+
+"""
+#メッセージ削除 いらない予感・・・
+@app.route("/message_delete/<key_urlsafe:.*>")
+class message_delete(webapp2.RequestHandler):
+	def get(self, key_urlsafe):
+		self.session = session(self.request)
+
+		k = ndb.Key(urlsafe=key_urlsafe)
+		m = k.get()
+		#自分へのメッセージだったら削除可能(念のため）
+		if m.to_player == self.session['plkey']:
+			m.key.delete()
+		#この設計だと削除しちゃうと送った方も見れなくなる☆
+
+		self.session.save()
+		return webapp2.Response( json.dumps( self.session ) )
+"""
+
+#ステージデータを配信
+@app.route("/game_stagestart")
+class game_stagestart(webapp2.RequestHandler):
+	def get(self):
+		self.session = session(self.request)
+
+		self.session.save()
+		return webapp2.Response( json.dumps( self.session ) )
+
+#ステージデータをクリア報告
+@app.route("/game_stageclear")
+class game_stageclear(webapp2.RequestHandler):
+	def get(self):
+		self.session = session(self.request)
+
+		self.session.save()
+		return webapp2.Response( json.dumps( self.session ) )
+
+"""
+@app.route("/game/action/")
+class gameaction(webapp2.RequestHandler):
+	def get(self):
+		self.session = session(self.request)
+
+		self.session.save()
+		return webapp2.Response( json.dumps( self.session ) )
+"""
+
+@app.route("/")
+class root(webapp2.RequestHandler):
+	def get(self):
+		return
+
 
